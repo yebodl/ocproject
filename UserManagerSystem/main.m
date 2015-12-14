29924ea5e56718f24872a4cd0c0245b446f2c89b
yebodl <843016@qq.com>
2015-12-14 13:22:41 -0800

    view and models
    view and models

---
+++ UserManagerSystem/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UserManagerSystem
+//
+//  Created by qianfeng on 15/12/14.
+//  Copyright (c) 2015年 YB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
